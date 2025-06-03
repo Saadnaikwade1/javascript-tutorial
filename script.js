21a07f68e94a6d7e190cb19cd2162275089d56c4
Saadnaikwade1 <naikwadesaad@gmail.com>
2025-06-03 11:18:49 +0530

    Object
    Object literal and spread methods

--- script.js
+++ script.js
@@ -1,70 +1,74 @@
-// /* Array Basics*/
-// let x;
-// //array literal
-// const numbers = [1, 2, 3, 4, 5];
-// //array constructor
-// const fruits = new Array("apple", "banana", "cherry");
-// x = numbers[0];
-// x = numbers[1] + numbers[2];
-// x = `my favarate fruit is ${fruits[1]}`;
+//object literal
+const person = {
+  name: "john Deo",
+  age: 30,
+  isAdmin: true,
+  adress: {
+    street: "123 main",
+    city: "baston",
+    state: "MA",
+  },
+  hobbies: ["music", "sports"],
+};
 
-// x = numbers.length;
-// fruits[2] = "pear";
-// x = fruits;
-// // fruits.length=2
-// fruits[3] = "stawberry";
-// fruits[fruits.length] = "mosambi";
-// fruits[fruits.length] = "blue beryy";
-// console.log(x);
-
-// //array methods
-// const arr = [34, 55, 32, 53, 54, 90];
-// arr.push(100);
-// arr.pop();
-// arr.unshift(99);
-// arr.shift();
-// arr.reverse();
-// x = arr.includes(34);
-// x = arr.indexOf(340); //if not present it will give return -1
-// // x=arr.slice(1,4)
-// // x=arr.splice(1,4)
-// x = arr.splice(1, 4).reverse().toString().charAt(0);
-// // console.log(x);
-
-//nesting,concatinating,spread oprator
 let x;
-const fruits = ["apple", "pear", "orange"];
-const berries = ["stawberry", "blueberry", "rasberry"];
-// fruits.push(berries);
-// x = fruits[3][1];
-const allFruits = [fruits, berries];
-x = allFruits[1][0];
+x = person.name;
+x = person["age"];
+x = person.adress.city;
+x = person.hobbies;
 
-//concating arr
-x = fruits.concat(berries);
+person.name = "jane doe";
+person["isAdmin"] = false;
 
-//spred oprator(...)
-x = [...fruits, ...berries];
+delete person.age;
+person.haschildren = true;
 
-// //array flatten
-// // const arr = [1, 2, [3, 4], 5, [6, 7], 8, 9];
-// x = arr.flat();
+person.greet = function () {
+  console.log(`Hello, My name is ${this.name}`);
+};
+person.greet();
+const person2 = {
+  "first name": "Brad",
+  "last name": "Traversy",
+};
 
-// //static method on array object
-// x = Array.isArray('hello');
-// x=Array.from('12345')
+x = person2["first name"];
+console.log(x);
 
-// const a=1;
-// const b=2;
-// const c=3;
-// x=Array.of(a,b,c);
+//spred and method of object
+let y;
+const todo = new Object();
+todo.id = 1;
+todo.name = "butter milk";
+todo.complited = false;
 
-// console.log(x);
+//nested object litrral
+const person3 = {
+  adress: {
+    coord: {
+      lat: -4.2323,
+      lang: 2.34,
+    },
+  },
+};
+y = person3.adress.coord.lang;
 
-// challenge1
+const obj1 = { a: 1, b: 2 };
+const obj2 = { c: 3, d: 4 };
+const obj3 = { ...obj1, ...obj2 };
+const obj4 = Object.assign({}, obj1, obj2);
+y = obj4;
 
-const arr = [1,2,3,4,5];
-arr.push(6);
-arr.unshift(0);
-arr.reverse()
-console.log(arr);
+const todos = [
+  { id: 1, name: "Buy Milk" },
+  { id: 2, name: "Pickup kids from school" },
+  { id: 3, name: "out of trash" },
+];
+y = todos[0].name;
+y = Object.keys(todo);
+y = Object.keys(todo).length;
+y=Object.values(todo);
+y=Object.entries(todo);
+y=Object.hasOwn('age')
+
+console.log(y);
