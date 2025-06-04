ad36fb1876d7b53c82c0550fc97edaa3924f4fa3
Saadnaikwade1 <naikwadesaad@gmail.com>
2025-06-04 16:46:12 +0530

    object exercise

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
-//json(javascript object Notaion)
-const post = {
-  id: 1,
-  titile: "Post one",
-  body: "this is the body",
-};
-//convet to Json string
-const str = JSON.stringify(post);
-console.log(str.id);
-
-//parse Json
-const obj = JSON.parse(str);
-console.log(obj.id);
-
-const posts = [
-  { id: 1, titile: "Post one", body: "this is the body" },
-  { id: 1, titile: "Post one", body: "this is the body" },
+// exersize
+const library = [
+  {
+    title: "The Road Ahead",
+    author: "Bill Gates",
+    status: {
+      own: true,
+      reading: false,
+      read: false,
+    },
+  },
+  {
+    title: "Rich Dad/Poor Dad",
+    author: "Robert kiyosaki",
+    status: {
+      own: true,
+      reading: false,
+      read: false,
+    },
+  },
+  {
+    title: "steve jobs",
+    author: "Weltor Issacson",
+    status: {
+      own: true,
+      reading: false,
+      read: false,
+    },
+  },
 ];
+//step2
+//set the read value to true
+library[0].status.read=true;
+library[1].status.read=true;
+library[2].status.read=true;
+console.log(library);
 
-const str2 = JSON.stringify(posts)
-console.log(str2);
+//step 3
+//destucuter the title from the first book and renme the title as firstBook
+const {title:firstBook}=library[0];
+console.log(firstBook);
+
+
+//turn the library object into the string
+const str=JSON.stringify(library);
+console.log(str);
