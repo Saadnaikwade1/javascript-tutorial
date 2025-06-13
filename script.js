1c88f2f62157820e35af36e0a591326bc3593ace
Saadnaikwade1 <naikwadesaad@gmail.com>
2025-06-13 22:01:39 +0530

    function basics

--- script.js
+++ script.js
@@ -1,46 +1,66 @@
-// exersize
-const library = [
-  {
-    title: "The Road Ahead",
-    author: "Bill Gates",
-    status: {
-      own: true,
-      reading: false,
-      read: false,
-    },
-  },
-  {
-    title: "Rich Dad/Poor Dad",
-    author: "Robert kiyosaki",
-    status: {
-      own: true,
-      reading: false,
-      read: false,
-    },
-  },
-  {
-    title: "steve jobs",
-    author: "Weltor Issacson",
-    status: {
-      own: true,
-      reading: false,
-      read: false,
-    },
-  },
-];
-//step2
-//set the read value to true
-library[0].status.read=true;
-library[1].status.read=true;
-library[2].status.read=true;
-console.log(library);
+function sayhello() {
+  console.log("hello world!");
+}
+sayhello();
 
-//step 3
-//destucuter the title from the first book and renme the title as firstBook
-const {title:firstBook}=library[0];
-console.log(firstBook);
+function add(num1, num2) {
+  console.log(num1 + num2);
+}
+add(2, 3);
+function substract(num1, num2) {
+  return num1 - num2;
+  console.log("helo");
+}
 
+const resut = substract(10, 5);
+console.log(resut);
 
-//turn the library object into the string
-const str=JSON.stringify(library);
-console.log(str);
+//parameter and argument
+function registorUser(user) {
+  // if (!user) {
+  //   user = "bot";
+  // }
+
+  return user + " is registered";
+}
+console.log(registorUser(" john"));
+
+//rest parameter
+function sum(...numbers) {
+  let total = 0;
+
+  for (const num of numbers) {
+    total += num;
+  }
+  return total;
+}
+
+console.log(sum(1, 2, 3));
+
+//object as params
+function loginUser(user) {
+  return `the user ${user.name} with the id of ${user.id} is logged in`;
+}
+const user = {
+  id: 1,
+  name: "john",
+};
+
+console.log(loginUser(user));
+console.log(
+  loginUser({
+    id: 2,
+    name: "david",
+  })
+);
+
+//array as params
+function getrandom(arr) {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+
+  const items = arr[randomIndex];
+
+  console.log(items);
+}
+
+getrandom([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
