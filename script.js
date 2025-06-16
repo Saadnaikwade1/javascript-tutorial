8cba91b3bca83f705d2d96a99a3d425c371ece41
Saadnaikwade1 <naikwadesaad@gmail.com>
2025-06-16 12:17:37 +0530

    truthy and falsy

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-// Calculator Challenge
-function calculator(num1, num2, oprator) {
-  let result;
-  switch (oprator) {
-    case "+":
-      result = num1 + num2;
-      break;
-    case "-":
-      result = num1 - num2;
-      break;
-    case "*":
-      result = num1 * num2;
-      break;
-    case "/":
-      result = num1 / num2;
-      break;
-    default:
-      result='Invalid Operator';
-  }
-  console.log(result);
-  return result;
+//truthy and falsy
+const x = "false";
+if (x) {
+  console.log("this is truthy");
+} else {
+  console.log("this is falsy");
 }
-calculator(2,3,'&');
+
+// Truthy and falsy conveats
+const children = 3;
+if (!isNaN(children)) {
+  console.log(`You have ${children} children`);
+} else {
+  console.log("please enter no of children");
+}
+
+// checking for empty Array
+const post = ["post one"];
+if (post.length > 0) {
+  console.log("List posts");
+} else {
+  console.log("No post to list");
+}
+
+//Checking for Empty object
+const user = { name:'Brad'};
+if (Object.keys(user).length > 0) {
+  console.log("LIST USER ");
+} else {
+  console.log("no list user");
+}
+
+// loose Equality(==)
+console.log(false === 0);
+console.log('' === 0);
+console.log(null === undefined);
