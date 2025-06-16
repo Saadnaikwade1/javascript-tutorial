4db4c76593d2c7007d0ff366faa417f4998d55f3
Saadnaikwade1 <naikwadesaad@gmail.com>
2025-06-16 11:42:48 +0530

    calculator challenge

--- script.js
+++ script.js
@@ -1,34 +1,23 @@
-//if else ,nested
-const d = new Date(10, 30, 25, 6, 0, 0);
-const hours = d.getHours();
-
-//if else
-if (hours < 12) {
-  console.log("Good Morning");
-} else if (hours < 18) {
-  console.log("good afternoon");
-} else {
-  console.log("Good Night");
-}
-
-//Nested if else
-if (hours < 12) {
-  console.log("Good Morning");
-  if (hours === 6) {
-    console.log("wake-up!");
+// Calculator Challenge
+function calculator(num1, num2, oprator) {
+  let result;
+  switch (oprator) {
+    case "+":
+      result = num1 + num2;
+      break;
+    case "-":
+      result = num1 - num2;
+      break;
+    case "*":
+      result = num1 * num2;
+      break;
+    case "/":
+      result = num1 / num2;
+      break;
+    default:
+      result='Invalid Operator';
   }
-} else if (hours < 18) {
-  console.log("good afternoon");
-} else {
-  console.log("Good Night");
-  if (hours >= 20) {
-    console.log("zzz");
-  }
+  console.log(result);
+  return result;
 }
-
-if (hours>=7 && hours <15){
-  console.log('Its Work Time');
-}
-if(hours===6 || hours===20){
-  console.log('brush your teeth!');
-}
+calculator(2,3,'&');
