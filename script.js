984871d4b54b5a60031e071b14f73e42b7ddf80a
Saadnaikwade1 <naikwadesaad@gmail.com>
2025-06-19 08:12:58 +0530

    Higher order method
    forEach,filter,map,reduce

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ socialObj.forEach((item) => {
 });
 
 //array.filter()
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // const evennumber = numbers.filter((num) => {
 //   return num % 2 === 0;
 // });
@@ -105,8 +105,67 @@ console.log(companyName);
 //create an with company catagory
 const companyinfo = companies1.map((company) => {
   return {
-    name:company.name,
-    category:company.category
+    name: company.name,
+    category: company.category,
   };
 });
 console.log(companyinfo);
+//create an array of an object with name and length of each company in years
+const companyYears = companies1.map((company) => {
+  return {
+    name: company.name,
+    length: company.end - company.start + " Years",
+  };
+});
+console.log(companyYears);
+
+//chain map method
+const squareAnddubel = nums.map((n) => Math.sqrt(n)).map((sqrt) => sqrt * 2);
+console.log(squareAnddubel);
+
+//brif
+const squareAnddubel2 = nums
+  .map(function (n) {
+    return Math.sqrt(n);
+  })
+  //below func takes the above sqrt of n as params
+  .map(function (sqrt) {
+    return sqrt * 2;
+  })
+  .map(function (sqrtduble) {
+    return sqrtduble * 3;
+  });
+console.log(squareAnddubel2);
+
+//chaining differet method
+const evenduoble = numbers.filter((n) => n % 2 === 0).map((even) => even * 2);
+console.log(evenduoble);
+
+//array.reduce()
+/*the .reduce() function is take previus value and current
+ value and initial value at last or starting */
+const numbers1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sum = numbers1.reduce(function (accumulator, currentvalue) {
+  return accumulator + currentvalue;
+}, 0); //initail value is 0
+console.log(sum);
+// const sum1 = numbers1.reduce((acc, cv) => acc + cv, 0);
+// console.log(sum1);
+
+//using for loop
+const sum3 = () => {
+  let acc = 0;
+  for (const cv of numbers1) {
+    acc = acc+cv;
+  }
+  return acc;
+};
+console.log(sum3());
+
+const cart=[
+  {id:1, name:'product 1', price:130},
+  {id:2, name:'product 2', price:150},
+  {id:3, name:'product 3', price:200},
+]
+const cartSum=cart.reduce((acc,cv)=> acc+cv.price,0);
+console.log(cartSum);
