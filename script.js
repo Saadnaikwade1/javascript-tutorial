d4d58c7843b1f603f990c342718c01e582da2c92
Saadnaikwade1 <naikwadesaad@gmail.com>
2025-06-19 09:10:49 +0530

    challenge

--- script.js
+++ script.js
@@ -1,171 +1,65 @@
-//Higher order array method
-const social = ["Twitter", "Facebook", "Linkedin", "Instagram"];
-
-// social.forEach(function(item) {
-//   console.log(item);
-// });
-
-//using arrow funtion
-// social.forEach((item)=>console.log(item));
-
-social.forEach((item, index, arr) => console.log(`${index}-${item}`, arr));
-
-function socialLogs(social) {
-  console.log(social);
-}
-social.forEach(socialLogs);
-
-const socialObj = [
-  { name: "Twitter", url: "https://twitter.com" },
-  { name: "Facebook", url: "https://facebook.com" },
-  { name: "Instagram", url: "https://instagram.com" },
-  { name: "Likedin", url: "https://linkedin.com" },
-];
-//array.forEach()
-socialObj.forEach((item) => {
-  console.log(item);
-});
-
-//array.filter()
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// const evennumber = numbers.filter((num) => {
-//   return num % 2 === 0;
-// });
-//shorter version
-// const evennumber = numbers.filter((num) => num % 2 === 0);
-
-//same with forEach()
-// const evennumber=[];
-// numbers.forEach((num)=>{
-//   if (num%2===0){
-//     evennumber.push(num);
-//   }
-// })
-// console.log(evennumber);
-
-const companies = [
-  { name: "companyone", category: "finance", start: 1981, end: 2004 },
-  { name: "companytwo", category: "Retail", start: 1992, end: 2008 },
-  { name: "companythree", category: "Auto", start: 1999, end: 2007 },
-  { name: "companyfour", category: "Retail", start: 1989, end: 2010 },
-  { name: "companyfive", category: "Technogy", start: 2009, end: 2014 },
-  { name: "companysix", category: "finance", start: 1987, end: 2010 },
-  { name: "companyseven", category: "Auto", start: 1986, end: 1996 },
-  { name: "companyeight", category: "Technogy", start: 2011, end: 2016 },
-  { name: "companynine", category: "Retail", start: 1981, end: 1989 },
-];
-//Get only Retail companies:
-const retailCompanies = companies.filter(
-  (company) => company.category === "Retail"
-);
-console.log(retailCompanies);
-
-//get the companies detail that started in or after 1980 and ended
-//in or before 2005
-const earlycompanies = companies.filter(
-  (company) => company.start >= 1980 && company.end <= 2005
-);
-console.log(earlycompanies);
-
-//companies that lasted 10 or more
-const longcompanies = companies.filter(
-  (company) => company.end - company.start >= 10
-);
-console.log(longcompanies);
-
-//array.map()
-/*The map() method of Array instances creates a new array populated
-with the results of calling a provided function on every element in 
- the calling array*/
-
-const nums = [1, 2, 3, 4, 5];
-const doubleNumbers = nums.map((n) => n * 2);
-console.log(doubleNumbers);
-
-//same with forEach()
-const doubleNumbers2 = [];
-nums.forEach((n) => doubleNumbers2.push(n * 2));
-console.log(doubleNumbers2);
-const companies1 = [
-  { name: "companyone", category: "finance", start: 1981, end: 2004 },
-  { name: "companytwo", category: "Retail", start: 1992, end: 2008 },
-  { name: "companythree", category: "Auto", start: 1999, end: 2007 },
-  { name: "companyfour", category: "Retail", start: 1989, end: 2010 },
-  { name: "companyfive", category: "Technogy", start: 2009, end: 2014 },
-  { name: "companysix", category: "finance", start: 1987, end: 2010 },
-  { name: "companyseven", category: "Auto", start: 1986, end: 1996 },
-  { name: "companyeight", category: "Technogy", start: 2011, end: 2016 },
-  { name: "companynine", category: "Retail", start: 1981, end: 1989 },
+/*Take the peole array and create an array called youngPeople
+  that stores object with ONLY 'name' and 'email' propeties of 
+  all the people that are 25 and under. The name property should
+  have there first name and last name */
+const people = [
+  {
+    firstName: "Jhon",
+    lastName: "Doe",
+    email: "john@gmail.com",
+    phone: "111-111-111",
+    age: 30,
+  },
+  {
+    firstName: "Jane",
+    lastName: "Poe",
+    email: "jane@gmail.com",
+    phone: "222-222-222",
+    age: 25,
+  },
+  {
+    firstName: "Bob",
+    lastName: "Foe",
+    email: "Bob@gmail.com",
+    phone: "333-333-333",
+    age: 45,
+  },
+  {
+    firstName: "Sare",
+    lastName: "Soe",
+    email: "max@gmail.com",
+    phone: "444-444-444",
+    age: 19,
+  },
+  {
+    firstName: "Jose",
+    lastName: "Koe",
+    email: "sare@gmail.com",
+    phone: "555-555-555",
+    age: 23,
+  },
 ];
 
-//create an new array of comapany name
-const companyName = companies1.map((company) => company.name);
-console.log(companyName);
+const youngPeople = people
+  .filter((person) => person.age <= 25)
+  .map((person) => ({
+    name: person.firstName + " " + person.lastName,
+    email: person.email,
+  }));
+console.log(youngPeople);
 
-//create an with company catagory
-const companyinfo = companies1.map((company) => {
-  return {
-    name: company.name,
-    category: company.category,
-  };
+//challenge add onlly positive no from the array
+const numbers = [2, -30, 50, 20, -12, -9, 7];
+const positive = numbers
+  .filter((number) => number > 0)
+  .reduce((acc, number) => acc + number, 0);
+console.log(positive);
+
+//challenge 3
+/* create a new array called capitalizedWord with word from the words array
+with the fisrt word of each letter is capitalized*/
+const words = ["coder", "programmer", "developer"];
+const cword = words.map((word) => {
+  return word[0].toLowerCase() + word.slice(1,word.length);
 });
-console.log(companyinfo);
-//create an array of an object with name and length of each company in years
-const companyYears = companies1.map((company) => {
-  return {
-    name: company.name,
-    length: company.end - company.start + " Years",
-  };
-});
-console.log(companyYears);
-
-//chain map method
-const squareAnddubel = nums.map((n) => Math.sqrt(n)).map((sqrt) => sqrt * 2);
-console.log(squareAnddubel);
-
-//brif
-const squareAnddubel2 = nums
-  .map(function (n) {
-    return Math.sqrt(n);
-  })
-  //below func takes the above sqrt of n as params
-  .map(function (sqrt) {
-    return sqrt * 2;
-  })
-  .map(function (sqrtduble) {
-    return sqrtduble * 3;
-  });
-console.log(squareAnddubel2);
-
-//chaining differet method
-const evenduoble = numbers.filter((n) => n % 2 === 0).map((even) => even * 2);
-console.log(evenduoble);
-
-//array.reduce()
-/*the .reduce() function is take previus value and current
- value and initial value at last or starting */
-const numbers1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const sum = numbers1.reduce(function (accumulator, currentvalue) {
-  return accumulator + currentvalue;
-}, 0); //initail value is 0
-console.log(sum);
-// const sum1 = numbers1.reduce((acc, cv) => acc + cv, 0);
-// console.log(sum1);
-
-//using for loop
-const sum3 = () => {
-  let acc = 0;
-  for (const cv of numbers1) {
-    acc = acc+cv;
-  }
-  return acc;
-};
-console.log(sum3());
-
-const cart=[
-  {id:1, name:'product 1', price:130},
-  {id:2, name:'product 2', price:150},
-  {id:3, name:'product 3', price:200},
-]
-const cartSum=cart.reduce((acc,cv)=> acc+cv.price,0);
-console.log(cartSum);
+console.log(cword);
