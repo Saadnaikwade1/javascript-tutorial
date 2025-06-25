d8696d8e8eb75e77ab1260c883802154073ece89
Saadnaikwade1 <naikwadesaad@gmail.com>
2025-06-25 08:12:30 +0530

    working with style and classses

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
-function removeClearButton() {
-  document.querySelector("#clear").remove();
+//working with style and classes
+const text = document.querySelector(".card");
+const itemlist = document.querySelector(".item-list");
+const item = itemlist.querySelectorAll("li");
+function run() {
+  //classname
+  // console.log(itemlist.className);
+  // text.className = "card dark";
+
+  //classList
+  // console.log(itemlist.classList);
+
+  itemlist.classList.forEach((c)=> console.log(c))
+  // text.classList.add('dark')
+  // text.classList.remove('card')
+  // text.classList.toggle('hidden')
+  text.classList.replace('card','dark')
+
+  //change style
+  // itemlist.style.lineHeight='3'
+
+  item.forEach((item,index)=>{
+    item.style.color='red'
+    if(index==2){
+      item.style.color='green'
+    }
+  })
 }
 
-function removeFirstItem() {
-  const ul = document.querySelector("ul");
-  const li = document.querySelector("li:first-child");
-  ul.removeChild(li);
-}
-function removeItem(itemNumber) {
-  const ul = document.querySelector("ul");
-  const li = document.querySelector(`li:nth-Child(${itemNumber})`);
-  ul.removeChild(li);
-}
-function removeItem2(itemNumber) {
-  const ul = document.querySelector("ul");
-  const li = document.querySelectorAll("li")[itemNumber-1];
-  ul.removeChild(li);
-}
-
-function removeItem3(itemNumber){
-   const li = document.querySelectorAll("li");
-   li[itemNumber-1].remove();
-}
-const removeItem4=((itemNumber)=>
-document.querySelectorAll('li')[itemNumber-1].remove())
-removeClearButton();
-// removeFirstItem();
-// removeItem();
-// removeItem2();
-// removeItem3();
-removeItem4()
+document.querySelector("button").onclick = run;
