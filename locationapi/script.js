4210049964a65ca7e48188c4b752922db9c54765
saad naikwade <naikwadesaad@gmail.com>
2025-08-06 22:14:39 +0530

    object literals

---
+++ locationapi/script.js
@@ -0,0 +1,17 @@
+//this code below for map copied from the leafletjs.com
+const map = L.map("map").setView([0, 0], 2);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+const marker = L.marker([0, 0]).addTo(map);
+
+navigator.geolocation.getCurrentPosition(function (pos) {
+  const lat = pos.coords.latitude;
+  const lag = pos.coords.longitude;
+
+  marker.setLatLang([lat, lag]).update();
+  map.setView([lat, lag], 13);
+});
---
+++ oops/script.js
@@ -0,0 +1,11 @@
+const rectangle= {
+    name:'Rectangle 1',
+    width:20,
+    height:10,
+    area: function(){
+        return this.width*this.height;
+
+    },
+};
+console.log(rectangle.area());
+console.log
