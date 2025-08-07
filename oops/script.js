27202c17f22e0a4708764a8763b1f944ab789eea
saad naikwade <naikwadesaad@gmail.com>
2025-08-07 12:46:32 +0530

    oops prototype

--- oops/script.js
+++ oops/script.js
@@ -1,11 +1,25 @@
-const rectangle= {
-    name:'Rectangle 1',
-    width:20,
-    height:10,
-    area: function(){
-        return this.width*this.height;
+function Rectangle(name, height, width) {
+  (this.name = name), (this.height = height), (this.width = width);
+}
 
-    },
+Rectangle.prototype.area = function () {
+  return this.height * this.width;
 };
-console.log(rectangle.area());
-console.log
+Rectangle.prototype.perimeter = function () {
+  return 2 * (this.height + this.width);
+};
+
+Rectangle.prototype.isSquare = function () {
+  return this.width === this.height;
+};
+Rectangle.prototype.name = function (newName) {
+  return (this.name = newName);
+};
+
+const rect1 = new Rectangle("rectangle 1", 10, 20);
+const rect2 = new Rectangle("rectangle 2", 20, 20);
+console.log(rect1);
+console.log(rect1.area());
+console.log(rect1.perimeter());
+console.log(rect1.isSquare());
+console.log(rect1.name);
